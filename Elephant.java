012c28a51fd316c43bec347e550645c66843b4d9
Daniel-Tann <340883792@gapps.yrdsb.ca>
2022-05-31 16:06:19 -0400

    Added Backround and added sound effects(some reason sounds won't play)

--- Elephant.java
+++ Elephant.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    GreenfootSound yumBanana = new GreenfootSound("banana.m4a");
+
     /**
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -42,6 +44,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.increaseScore();
             world.createBanana();
+            yumBanana.play();
         }
     }
 }
